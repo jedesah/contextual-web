22a7c07d631be7eeea5cfd4e3355e503115fcda2
Jon Pretty <jon.pretty@propensive.com>
2016-12-13 14:06:21 +0000

    Added google analytics

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -83,7 +83,8 @@ object Page {
           Meta(name = 'description, content = ""),
           Meta(name = 'author, content = ""),
           Meta(name = 'viewport, content = "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0"),
-          Style(typ = "text/css")(Contextual.styles())
+          Style(typ = "text/css")(Contextual.styles()),
+          Script("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-60044672-3','auto');ga('send','pageview');")
         ),
         Body(
           Div(cls = 'header)(
